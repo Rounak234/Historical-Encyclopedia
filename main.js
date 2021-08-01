d5d08d0104ffcddc410b9ab73513fb6b94af4aef
Rounak234 <88239475+Rounak234@users.noreply.github.com>
2021-08-01 12:36:59 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+
+<script type="tet/javascript">
+
+
+<div id="Front">
+
+
+
+			<H1><em>Welcome to Encyclopedia World</em></H1>
+
+			<p id="Fantastic World">Click button given below</p>
+			<img id="front-img" src="image4.jpg">
+
+			<p3><button onclick="open_encyclopedia()"><a href="Open Encyclopedia">Open Enyclopedia</button></div>
+
+				<div id="back">
+					<h1><em>Enjoy Reading</em></h1>
+					<p id="Unknown World">Are you Excited?</p>
+
+					<img id="back_img" src="image2.jpg">
+
+					<p><button onclick="close_encyclopedia()">Click to Read Again</button></div>
+
+					</script>
